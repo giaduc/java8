00bc430b581514dc9f6c3c4d7b1a9c02fb504ef1
giaduc <lgduc1913@gmail.com>
2018-08-07 16:43:46 +0700

    SortWithLamda - sort by name

--- src/comparator/SortWithLamda.java
+++ src/comparator/SortWithLamda.java
@@ -5,21 +5,24 @@ import java.util.List;
 public class SortWithLamda {
 
 	public static void main(String[] args) {
-		
+
 		List<Deverloper> deverlopers = Deverloper.getDeverlopers();
-		
+
 		System.out.println("Before sort...");
 		for (Deverloper d : deverlopers) {
 			System.out.println(d);
 		}
+
+		// sort by age with lamda
+		deverlopers.sort((d1, d2) -> d1.getAge() - d2.getAge());
 		
-		// sort with lamda
-		deverlopers.sort((Deverloper d1, Deverloper d2) -> d1.getAge() - d2.getAge());
-		
+		// sort by name with lamda
+		deverlopers.sort((d1, d2) -> d1.getName().compareTo(d2.getName()));
+
 		System.out.println("After sort...");
 		// java 8 only, print the list
 		deverlopers.forEach(System.out::println);
-		
+
 	}
-	
+
 }
